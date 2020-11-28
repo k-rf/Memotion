3bd815062f47488589ea2f77fdcd2d2accd45190
Kawano Kenichi <bppk28@gmail.com>
2020-11-29 08:50:09 +0900

    :sparkles: Add container for backend

--- backend/memotion/__init__.py
+++ backend/memotion/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "Hello"}
